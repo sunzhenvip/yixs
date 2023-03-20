4753439ae323d7676ef5aa504410941448a27bf8
sunzhen <sunzhen@jvsit.com>
2023-03-20 13:56:42 +0800

    Changes

--- test.py
+++ test.py
@@ -1,8 +1,52 @@
 import json
 import logging
 import datetime
+import os
 from logging.handlers import RotatingFileHandler
-from logging.handlers import TimedRotatingFileHandler
+
+
+# import time
+# from logging.handlers import TimedRotatingFileHandler
+
+
+class TimeRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, filename, mode='a', maxBytes=0, backupCount=0,
+                 encoding=None, delay=0):
+        super().__init__(filename, mode, maxBytes, backupCount, encoding, delay)
+        self.prefix = os.path.splitext(filename)[0]
+
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+
+        # 获取当前日期和时间，并格式化为可读字符串
+        now = datetime.datetime.now()
+        date_string = now.strftime("%Y%m%d_%H%M%S")
+
+        # 根据日期生成新的日志文件名
+        new_log_file_name = f'{self.prefix}_{date_string}.log'
+
+        # 如果备份数量大于0，则进行文件轮换
+        if self.backupCount > 0:
+            for i in range(self.backupCount - 1, 0, -1):
+                src = f"{self.prefix}_{i}.log"
+                dst = f"{self.prefix}_{i + 1}.log"
+                if os.path.exists(src):
+                    os.rename(src, dst)
+            dst = f"{self.prefix}_1.log"
+            if os.path.exists(self.prefix + ".log"):
+                os.rename(self.prefix + ".log", dst)
+
+        # 打开新的日志文件
+        self.mode = 'w'
+        self.stream = self._open()
+
+    def _open(self):
+        if self.encoding is None:
+            return open(self.baseFilename, self.mode)
+        else:
+            return open(self.baseFilename, self.mode, encoding=self.encoding)
 
 
 class JsonFilter(logging.Filter):
@@ -37,8 +81,9 @@ if __name__ == '__main__':
     date_string = now.strftime("%Y%m%d_%H%M%S")
     max_bytes = 1 * 1024 * 1024  # 1 MB
     backup_count = 5
-    handler = RotatingFileHandler(
-        filename=f'attachment/test_{date_string}.log',  # 在文件名中添加日期和时间戳信息
+    handler = TimeRotatingFileHandler(
+        # filename=f'attachment/test_{date_string}.log',  # 在文件名中添加日期和时间戳信息
+        filename=f'attachment/test.log',  # 在文件名中添加日期和时间戳信息
         mode='a',  # append to existing file
         maxBytes=max_bytes,
         backupCount=backup_count
